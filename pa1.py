9c37df5452ee953680bdf6579b0ba626d0705afb
mattdonroth <mroth@sandiego.edu>
2018-02-18 15:57:12 -0800

    Added all of the comments
    Submitting the file

--- pa1.py
+++ pa1.py
@@ -1,66 +1,67 @@
+'''
+Automata Project 1 DFA sim
+A simple python program to emulate a DFA based on the given input file
+Matthew Roth: Mroth@sandiego.edu
+Jack Miller: Jackmiller@sandiego.edu
+2-18-2018
+'''
 import sys
 
+#Our function that scans the input string and emulates the DFA symbol by symbol
 def runMachine(dfa, start, accept, string):
     state = str(start)
-    for char in string:
+    for symbol in string:
         try:
-            state = dfa[state][char]
+            state = dfa[state][symbol]
         except:
-            print("Could not find key: ", char, " in state: ", state)
+            print("Could not find key: ", symbol, " in state: ", state)
     return str(state)
 
-#declaring out dfa representation
+#Declaring our dfa representation
 dfa_dic = {}
-#opening the input
+#Opening the input
 input = tuple(open(sys.argv[1], "r"))
-#pulling the number of states
+#Pulling the number of states
 num_states = int(input[0])
-#pulling our alphabet
+#Pulling and cleaning our alphabet
 alphabet = input[1].strip()
-#getting the number of transitions in our automata
+#Getting the number of transitions in our DFA
 num_transitions = num_states*len(alphabet)
-#creating an iterable so we can skip the first two lines
+#Creating an iterable so we can skip the first two lines
 iterinput = iter(input)
 next(iterinput)
 next(iterinput)
-#error checking
-print("number of states", num_states)
-print("the alphabet", alphabet)
-#count is used to stop inputing the transitions when we reach the start and accept states
+#Count is used to stop taking in the transitions when we reach the start and accept states
 count = 0
-print("NUM TRANSITIONS:", num_transitions)
 for line in iterinput:
     if(count < num_transitions):
         count+=1
-        #splitting the transition
-        a,b,c = line.split()
-        a = a.replace('\'', '')
-        b = b.replace('\'', '')
-        c = c.replace('\'', '')
-        #adding the transition to our 2d dictionary
-        dfa_dic[a] = dfa_dic.get(a, {})
-        dfa_dic[a][b] = c
+        #Splitting the transition
+        curState,symbol,nextState = line.split()
+        #Cleaning our strings
+        curState = curState.replace('\'', '')
+        symbol = symbol.replace('\'', '')
+        nextState = nextState.replace('\'', '')
+        #Adding the transition to our 2d dictionary
+        dfa_dic[curState] = dfa_dic.get(curState, {})
+        dfa_dic[curState][symbol] = nextState
         continue
     else:
-        #setting the start state
+        #Setting the start state
         start_state = int(line)
         break
-#grabbing the accept states
+#Grabbing the accept states
 accept_states = next(iterinput)
-print("Start state", start_state)
-print("accept states", accept_states)
-print("running the machine")
-#iterating through the strings
+#Iterating through the strings
 for string in iterinput:
+    #Cleaning the input string
     string = string.strip()
-    #print("attempting to run string: ", string)
+    #Running our machine
     ans = runMachine(dfa_dic, start_state, accept_states, string)
     if ans in accept_states:
         print("Accept")
     else:
         print("Reject")
 
-#error checking
-#print(dfa_dic)
 
 
